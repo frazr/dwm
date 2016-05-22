6c5fdc9936a0c5f0f2dccff86bc92433205898dc
root <root@home.alltele.net>
2016-05-22 02:45:12 +0200

    No more xterm

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const char *fonts[] = {
-	"monospace:size=8"
+	"terminus:size=9"
 };
 static const char dmenufont[]       = "-*-terminus-medium-r-*-*-10-*-*-*-*-*-*-*";
 static const char normbordercolor[] = "#444444";
@@ -11,14 +11,14 @@ static const char normfgcolor[]     = "#bbbbbb";
 static const char selbordercolor[]  = "#005577";
 static const char selbgcolor[]      = "#005577";
 static const char selfgcolor[]      = "#eeeeee";
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool focusonwheelscroll = False;
 
 /* tagging */
-static const char *tags[] = { "irc", "dev", "web", "misc" };
+static const char *tags[] = { "web", "vim", "mail", "irc" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -26,7 +26,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	/*{ "Gimp",     NULL,       NULL,       0,            1,           -1 },*/
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
@@ -44,6 +44,8 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
+#define MODKEY2 Mod1Mask
+
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -55,12 +57,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-//"dmenu_run", "-fn", dmenufont, "-nb"
-//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-//static const char *dmenucmd[] = { "dmenu_run", "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "xterm", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *thunarcmd[]  = { "thunar", NULL };
 
 static Key keys[] = {
@@ -69,7 +67,10 @@ static Key keys[] = {
          { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
          { MODKEY,                       XK_e,      spawn,          {.v = thunarcmd } },
          { MODKEY,                       XK_b,      togglebar,      {0} },
+
          { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+         { MODKEY2,                      XK_Tab,    focusstack,     {.i = +1 } },
+
          { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
          { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
          { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
@@ -85,10 +86,10 @@ static Key keys[] = {
          { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
          { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
          { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-         { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-         { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-         { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-         { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+         { MODKEY,                       XK_comma,  focusmon,       {.i = +1 } },
+         { MODKEY,                       XK_period, focusmon,       {.i = -1 } },
+         { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = +1 } },
+         { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = -1 } },
          TAGKEYS(                        XK_1,                      0)
          TAGKEYS(                        XK_2,                      1)
          TAGKEYS(                        XK_3,                      2)
